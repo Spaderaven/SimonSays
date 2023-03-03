04f9c16838879969b449ecb62d0cc8eefa2b4185
Chobi <watsit777@gmail.com>
2023-03-03 02:34:38 +0000

    Working Simon
    Just has some comments and a bit of code clean up needed

--- assignment-02.js
+++ assignment-02.js
@@ -1,8 +1,11 @@
 let simon = []
-const player = []
+let simonSpeaking = false;
 let playerPress = false;
 let totalSimon = 0;
 let playerCurrent = 0;
+let interID;
+let waitTime = 1000;
+let finish = false;
 
 function getRandom() {
     return Math.floor(Math.random() * 4);
@@ -14,9 +17,10 @@ const sleep = async (milliseconds) => {
     });
 };
 
-function StartGame() {
+async function StartGame() {
     simon = [];
     document.getElementById("led").style.backgroundColor = "lightgreen";
+    await sleep(3000);
     AddSimon();
 }
 
@@ -29,16 +33,24 @@ function AddSimon() {
 
 const SimonSpeak = async () => {
 
+    simonSpeaking = true;
+
     for (let i = 0; i < simon.length; i++) {
         color = simon[i]
-        if (color == 0) FlashGreen();
-        else if (color == 1) FlashRed();
-        else if (color == 2) FlashYellow();
-        else if (color == 3) FlashBlue();
-        await sleep (1000)
+        if (color == 0) await FlashGreen();
+        else if (color == 1) await FlashRed();
+        else if (color == 2) await FlashYellow();
+        else if (color == 3) await FlashBlue();
+
+        if(i != (simon.length-1)){
+            await sleep (waitTime)
+        }
     }
 
     console.log("MY TUUUURN");
+    playerCurrent = 0;
+    playerPress = false;
+    simonSpeaking = false;
     PlayerSpeak();
 }
 
@@ -71,18 +83,15 @@ async function FlashBlue() {
 }
 
 function PlayerSpeak() {
-    let interID = setInterval(() => {
 
+    console.log("FINSHD?", finish);
+    if(finish) return;
 
-
-        if(playerPress) {
-            playerPress = false;
-            if(!ColorsRight()) {
-                clearInterval(interID);
-                EndGame();
-            }
-        }
-        else {
+    console.log("FINSHD?", finish);
+    interID = setInterval(() => {
+        if(!playerPress) {
+            console.log("CANCELD INTERVAL");
+            console.log("NEW ID   ", interID);
             clearInterval(interID);
             EndGame();
         }
@@ -90,28 +99,84 @@ function PlayerSpeak() {
 }
 
 
-function ColorsRight() {
-    for (let i = 0; i < player.length; i++) {
-        if(simon[i] != player[i]) return false;
-    }
-
-    return true;
-}
-
-
 function EndGame() {
     document.getElementById("led").style.backgroundColor = "red";
+    document.getElementById("display2").innerHTML = "00"
+    FlashEnding();
     console.log("FIIIIIIIIIIIIN");
+    console.log("DEAD ID", interID);
+    clearInterval(interID);
+
+    topScore = parseInt(document.getElementById("display1").innerHTML)
+    console.log("TIP SOCRE", topScore);
+    lastScore = totalSimon-1;
+
+    if (lastScore > topScore) {
+        let score = lastScore >= 10 ? lastScore : "0" + lastScore;
+        document.getElementById("display1").innerHTML = score;
+    }
+
+    
+    playerCurrent = 0;
+    simon = [];
+    playerPress = false;
+    totalSimon = 0;
+}
+
+const FlashEnding = async () => {
+
+    for (let i = 0; i < 5; i++) {
+        document.getElementById("top-left").style.backgroundColor = "lightgreen";
+        document.getElementById("top-right").style.backgroundColor = "lightpink";
+        document.getElementById("bottom-left").style.backgroundColor = "lightgoldenrodyellow";
+        document.getElementById("bottom-right").style.backgroundColor = "lightblue";
+        
+        await sleep(300)
+
+        document.getElementById("top-left").style.backgroundColor = "green";
+        document.getElementById("top-right").style.backgroundColor = "red";
+        document.getElementById("bottom-left").style.backgroundColor = "yellow";
+        document.getElementById("bottom-right").style.backgroundColor = "blue";
+
+        await sleep(300)
+    }
 }
 
 
-function ButtonPressed (button) {
-    player.push(button);
-    console.log("PLAYER: ", player);
-    if(button == simon[playerCurrent]){
+
+async function ButtonPressed (button) {
+    finish = false;
+
+    if(simonSpeaking)
+    {
+        clearInterval(interID);
+        EndGame();
+        finish = true;
+    }
+
+    if(button == simon[playerCurrent]) {
+
+        if(totalSimon == 5 || totalSimon == 9 || totalSimon == 13)
+        {
+            waitTime = waitTime - 250;
+        }
+
+        clearInterval(interID);
+        PlayerSpeak();
         playerCurrent++;
+    } 
+    else{
+        EndGame();
+        finish = true;
+    } 
+
+    if(playerCurrent == totalSimon && !finish){
+        clearInterval(interID);
+        let score = playerCurrent >= 10 ? playerCurrent : "0" + playerCurrent;
+        document.getElementById("display2").innerHTML = score;
+        playerCurrent = 0;
+        await sleep(800);
         AddSimon();
     }
-    else
-        EndGame();
+
 }
